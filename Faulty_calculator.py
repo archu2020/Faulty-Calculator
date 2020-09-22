b17ce15bc877137bd7da9bc6fbcc4b227488fd1a
Archana Mondal <71519727+archu2020@users.noreply.github.com>
2020-09-22 17:59:38 +0530

    Add files via upload

---
+++ Faulty_calculator.py
@@ -0,0 +1,36 @@
+# This programme is a faulty calculator all clculation will correct except fllowings this ones:
+# 4*55=687, 89-123=569,58+90=154,5696/82=954
+
+inp1=int(input("Please Enter your 1st Number\n"))
+inp2=input("Please Enter your Oparetor\n")
+inp3=int(input("Please Enter your 2nd Number\n"))
+
+if inp1 == 4 and inp3 == 55 and inp2 == "*":
+    print("The ans is 689")
+    print("This calculation is worng please try again")
+
+elif inp1 == 89 and inp3 == 123 and inp2 == "-":
+    print("The ans is 569")
+    print("This calculation is worng please try again")
+
+elif inp1 == 58 and inp3 == 90 and inp2 == "+":
+    print("The ans is 154")
+    print("This calculation is worng please try again")
+
+elif inp1 == 5696 and inp3 == 82 and inp2 == "/":
+    print("The ans is 945")
+    print("This calculation is worng please try again")
+
+elif inp2 == "*":
+    print("The Ans is",inp1 * inp3)
+
+elif inp2 == "-":
+    print("The Ans is",inp1 - inp3)
+
+elif inp2 == "+":
+    print("The Ans is",inp1 + inp3)
+
+elif inp2 == "/":
+    print("The Ans is",inp1 / inp3)
+else:
+    print("Worng input , Please try again latter")
