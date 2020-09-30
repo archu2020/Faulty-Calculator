f0bb19169c93938384e1f33929aa2315eb4f765f
Itsdudehere <69688840+Itsdudehere@users.noreply.github.com>
2020-09-30 22:50:10 +0530

    Update Faulty_calculator.py

--- Faulty_calculator.py
+++ Faulty_calculator.py
@@ -2,24 +2,24 @@
 # 4*55=687, 89-123=569,58+90=154,5696/82=954
 
 inp1=int(input("Please Enter your 1st Number\n"))
-inp2=input("Please Enter your Oparetor\n")
+inp2=input("Please Enter your Operator\n")
 inp3=int(input("Please Enter your 2nd Number\n"))
 
 if inp1 == 4 and inp3 == 55 and inp2 == "*":
     print("The ans is 689")
-    print("This calculation is worng please try again")
+    print("This calculation is wrong please try again")
 
 elif inp1 == 89 and inp3 == 123 and inp2 == "-":
     print("The ans is 569")
-    print("This calculation is worng please try again")
+    print("This calculation is wrong please try again")
 
 elif inp1 == 58 and inp3 == 90 and inp2 == "+":
     print("The ans is 154")
-    print("This calculation is worng please try again")
+    print("This calculation is wrong please try again")
 
 elif inp1 == 5696 and inp3 == 82 and inp2 == "/":
     print("The ans is 945")
-    print("This calculation is worng please try again")
+    print("This calculation is wrong please try again")
 
 elif inp2 == "*":
     print("The Ans is",inp1 * inp3)
@@ -33,4 +33,4 @@ elif inp2 == "+":
 elif inp2 == "/":
     print("The Ans is",inp1 / inp3)
 else:
-    print("Worng input , Please try again latter")
+    print("Wrong input , Please try again later")
